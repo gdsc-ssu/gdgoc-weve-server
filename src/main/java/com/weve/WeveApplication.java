e24ab0edcf8d9a661f4e2090a3b3390d96d6023e
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-02-03 17:34:40 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/weve/WeveApplication.java
@@ -0,0 +1,13 @@
+package com.weve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weve/WeveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
