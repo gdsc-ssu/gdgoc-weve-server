a3fbce433e06f9703be5c32be11a8239427f8c41
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-02-08 22:36:40 +0900

    feat: category mapping 생성 로직 추가

--- src/main/java/com/weve/domain/CategoryMapping.java
+++ src/main/java/com/weve/domain/CategoryMapping.java
@@ -4,23 +4,25 @@ import com.weve.domain.enums.HardshipCategory;
 import com.weve.domain.enums.JobCategory;
 import com.weve.domain.enums.ValueCategory;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CategoryMapping {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "worry_id", nullable = false)
+    @JoinColumn(name = "worry_id")
     private Worry worry;
 
     @ManyToOne
-    @JoinColumn(name = "senior_id", nullable = false)
+    @JoinColumn(name = "senior_id")
     private User senior;
 
     @Enumerated(EnumType.STRING)
