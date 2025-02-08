0e4c11df1e3ab1a0b756cfc22f90fda3eebb89cc
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-02-08 21:04:13 +0900

    refactor: 명명 규칙 반영(Enum 상수)

--- src/main/java/com/weve/domain/enums/HardshipCategory.java
+++ src/main/java/com/weve/domain/enums/HardshipCategory.java
@@ -1,9 +1,14 @@
 package com.weve.domain.enums;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum HardshipCategory {
-    LogicalProblemSolving,  // 논리적 해결형
-    EmotionalSupport,  // 감정적 공감형
-    PersistenceEndurance, // 끈기 인내형
-    CreativeTransformation, // 창의적 변환형
-    ImmediateAction // 즉각적 실행형
+    LOGICAL_PROBLEM_SOLVING("논리적 해결형"),
+    EMOTIONAL_SUPPORT("감정적 공감형"),
+    PERSISTENCE_ENDURANCE("끈기 인내형"),
+    CREATIVE_TRANSFORMATION("창의적 변환형"),
+    IMMEDIATE_ACTION("즉각적 실행형");
+
+    private final String description;
 }
