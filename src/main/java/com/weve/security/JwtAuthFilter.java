da8cd61e6d03bbdc2a02126b698547d4e07b1d13
shinminkyoung1 <dottiee99@naver.com>
2025-02-17 20:20:59 +0900

    refactor: 의존성 주입 어노테이션 추가

--- src/main/java/com/weve/security/JwtAuthFilter.java
+++ src/main/java/com/weve/security/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -16,15 +17,11 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
     private final UserRepository userRepository;
-
-    public JwtAuthFilter(JwtUtil jwtUtil, UserRepository userRepository) {
-        this.jwtUtil = jwtUtil;
-        this.userRepository = userRepository;
-    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
--- src/main/java/com/weve/service/AuthService.java
+++ src/main/java/com/weve/service/AuthService.java
@@ -6,21 +6,18 @@ import com.weve.domain.User;
 import com.weve.domain.enums.UserType;
 import com.weve.repository.UserRepository;
 import com.weve.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
 
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
-
-    public AuthService(UserRepository userRepository, JwtUtil jwtUtil) {
-        this.userRepository = userRepository;
-        this.jwtUtil = jwtUtil;
-    }
 
     // 회원가입
     public boolean register(String name, String phoneNumber, LocalDate birth, UserType userType) {
