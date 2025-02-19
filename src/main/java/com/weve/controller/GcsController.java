755b184cb711a7977737f7314a3732085b91030b
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-02-19 15:32:34 +0900

    feat: 파일 업로드 API 구현

---
+++ src/main/java/com/weve/controller/GcsController.java
@@ -0,0 +1,39 @@
+package com.weve.controller;
+
+import com.weve.common.api.payload.BasicResponse;
+import com.weve.dto.response.UploadFileResponse;
+import com.weve.service.GcsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@Slf4j
+@RequestMapping("/storage")
+public class GcsController {
+
+    private final GcsService gcsService;
+
+    @PostMapping("/upload")
+    public BasicResponse<UploadFileResponse> uploadFile(@RequestPart(value = "file") MultipartFile multipartFile) throws IOException {
+
+
+        String fileUrl = gcsService.processFile(gcsService.uploadFile(multipartFile));
+
+        UploadFileResponse response = UploadFileResponse
+                .builder()
+                .fileUrl(fileUrl)
+                .build();
+
+        return BasicResponse.onSuccess(response);
+    }
+}
---
+++ src/main/java/com/weve/dto/response/UploadFileResponse.java
@@ -0,0 +1,15 @@
+package com.weve.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UploadFileResponse {
+    private String fileUrl;
+}
+
