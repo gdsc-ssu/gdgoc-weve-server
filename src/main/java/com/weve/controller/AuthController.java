517f355a87bcf64d2076a8ca9b6b9fa77cda0321
shinminkyoung1 <dottiee99@naver.com>
2025-02-28 15:47:35 +0900

    refactor: 로그인 로직 수정

--- src/main/java/com/weve/controller/AuthController.java
+++ src/main/java/com/weve/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.weve.common.api.payload.BasicResponse;
 import com.weve.common.api.payload.code.status.ErrorStatus;
 import com.weve.dto.request.UserRequestDto;
 import com.weve.service.AuthService;
+import jakarta.persistence.Basic;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,12 +42,15 @@ public class AuthController {
     @PostMapping("/login")
     public BasicResponse<?> login(@RequestBody UserRequestDto request) {
 
-        // 전화번호 필수 체크
+        // 전화번호 및 이름 필수 체크
         if (request.getPhoneNumber() == null || request.getPhoneNumber().isBlank()) {
             return BasicResponse.onFailure(ErrorStatus._BAD_REQUEST.getCode(), "전화번호를 입력하세요.", null);
         }
+        if (request.getName() == null || request.getName().isBlank()) {
+            return BasicResponse.onFailure(ErrorStatus._BAD_REQUEST.getCode(), "이름을 입력하세요.", null);
+        }
 
-        String token = authService.login(request.getPhoneNumber());
+        String token = authService.login(request.getPhoneNumber(), request.getName());
 
         if (token == null) {
             return BasicResponse.onFailure(ErrorStatus.USER_NOT_FOUND.getCode(), "등록되지 않은 전화번호입니다.", null);
