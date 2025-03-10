8ea72eb9338d9ada75f427b8e449fabe8db237f7
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-03-10 20:39:14 +0900

    feat: health check & gitkeep 설정

---
+++ src/main/java/com/weve/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.weve.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @Hidden (스웨거 숨김)
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> HealthCheck() {
+        return ResponseEntity.ok("Health Check");
+    }
+}
