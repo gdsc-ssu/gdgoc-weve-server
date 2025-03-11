165fdf886cce6540e6a9a05b3915fee9bc65f9f8
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-03-11 13:10:45 +0900

    feat: Swagger 연동(controller, dto)

--- src/main/java/com/weve/dto/response/GetAnswerResponse.java
+++ src/main/java/com/weve/dto/response/GetAnswerResponse.java
@@ -1,6 +1,7 @@
 package com.weve.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,8 +15,11 @@ public class GetAnswerResponse {
     @AllArgsConstructor
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class JuniorVer {
+        @Schema(description = "답변 내용", nullable = false, example = "힘내요 젊은이..")
         private String content;
+        @Schema(description = "작성자", nullable = false, example = "베트남에 사는 71세 하니팜")
         private String author;
+        @Schema(description = "이미지 URL", nullable = true, example = "...")
         private String imageUrl;
     }
 
@@ -25,8 +29,11 @@ public class GetAnswerResponse {
     @AllArgsConstructor
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class SeniorVer {
+        @Schema(description = "답변 내용", nullable = false, example = "힘내요 젊은이..")
         private String content;
+        @Schema(description = "음성 파일 URL", nullable = false, example = "...")
         private String audioUrl;
+        @Schema(description = "이미지 URL", nullable = true, example = "...")
         private String imageUrl;
     }
 }
--- src/main/java/com/weve/dto/response/UploadFileResponse.java
+++ src/main/java/com/weve/dto/response/UploadFileResponse.java
@@ -1,5 +1,6 @@
 package com.weve.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UploadFileResponse {
+    @Schema(description = "파일 URL", nullable = false, example = "...")
     private String fileUrl;
 }
 
