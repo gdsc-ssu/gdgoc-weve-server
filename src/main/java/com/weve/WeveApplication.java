2dbcf62214e7f22365688eb715a4cafa5ae70cd5
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-03-11 11:13:39 +0900

    feat: Swagger 설정

--- src/main/java/com/weve/WeveApplication.java
+++ src/main/java/com/weve/WeveApplication.java
@@ -1,11 +1,19 @@
 package com.weve;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://gdg-weve.store", description = "Production Server"),
+                @Server(url = "http://localhost:8080", description = "Local Development Server")
+        }
+)
 public class WeveApplication {
 
     public static void main(String[] args) {
