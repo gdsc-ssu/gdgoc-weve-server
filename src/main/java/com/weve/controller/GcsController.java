32d58f422f6688ca566d895aa3b53f04c0075c4b
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-03-14 22:08:28 +0900

    refactor: consume 파라미터 추가

--- src/main/java/com/weve/controller/GcsController.java
+++ src/main/java/com/weve/controller/GcsController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,7 @@ public class GcsController {
 
     private final GcsService gcsService;
 
-    @PostMapping("/upload")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "파일 업로드", description = "파일을 업로드하여 URL을 생성합니다.")
     public BasicResponse<UploadFileResponse> uploadFile(@RequestPart(value = "file") MultipartFile multipartFile) throws IOException {
 
