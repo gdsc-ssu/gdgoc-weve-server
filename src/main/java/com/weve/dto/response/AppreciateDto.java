17d203f1ca8f83d3eddf241c0ac313fd832385b2
shinminkyoung1 <dottiee0702@gmail.com>
2025-03-25 02:17:15 +0900

    refactor: 감사편지 목록 조회 dto 분할

---
+++ src/main/java/com/weve/dto/response/AppreciateDto.java
@@ -0,0 +1,25 @@
+package com.weve.dto.response;
+
+import com.weve.domain.Appreciate;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class AppreciateDto {
+    private Long id;
+    private String content;
+    private LocalDateTime createdAt;
+    private boolean isRead;
+
+    public static AppreciateDto from(Appreciate appreciate) {
+        return AppreciateDto.builder()
+                .id(appreciate.getId())
+                .content(appreciate.getContent())
+                .createdAt(appreciate.getCreatedAt())
+                .isRead(appreciate.isRead())
+                .build();
+    }
+}
