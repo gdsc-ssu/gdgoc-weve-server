ef91da23679a48e2cd14ff4e7843b2f1c9ccf5a6
shinminkyoung1 <dottiee0702@gmail.com>
2025-04-04 19:38:35 +0900

    feat: 회원 가입 시 프로필 이미지 설정 추가 (시니어)

--- src/main/java/com/weve/service/AuthService.java
+++ src/main/java/com/weve/service/AuthService.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.deser.DataFormatReaders;
 import com.weve.common.api.payload.BasicResponse;
 import com.weve.domain.User;
 import com.weve.domain.enums.Language;
+import com.weve.domain.enums.ProfileColor;
 import com.weve.domain.enums.UserType;
 import com.weve.repository.UserRepository;
 import com.weve.security.JwtUtil;
@@ -16,8 +17,11 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static com.weve.domain.enums.UserType.SENIOR;
 
 @Service
 @RequiredArgsConstructor
@@ -38,6 +42,17 @@ public class AuthService {
         String countryCode = extractCountryCode(phoneNumber);
         String nationality = COUNTRY_NATIONALITY_MAP.getOrDefault(countryCode, "Unknown");
 
+        ProfileColor profileColor;
+        if (userType == SENIOR) {
+            // 시니어: ORANGE, BLUE, PINK 중 랜덤 선택
+            ProfileColor[] seniorColors = {ProfileColor.ORANGE, ProfileColor.BLUE, ProfileColor.PINK};
+            profileColor = seniorColors[new Random().nextInt(seniorColors.length)];
+        } else {
+            // 주니어: YELLOW, GREEN 중 랜덤 선택
+            ProfileColor[] juniorColors = {ProfileColor.YELLOW, ProfileColor.GREEN};
+            profileColor = juniorColors[new Random().nextInt(juniorColors.length)];
+        }
+
         User newUser = User.builder()
                 .name(name)
                 .phoneNumber(phoneNumber)
@@ -45,6 +60,7 @@ public class AuthService {
                 .userType(userType)
                 .language(language)
                 .nationality(nationality)
+                .profileColor(profileColor)
                 .build();
 
         userRepository.save(newUser);
